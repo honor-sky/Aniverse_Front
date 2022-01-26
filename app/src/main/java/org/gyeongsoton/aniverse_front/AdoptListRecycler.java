a621c21c21344bd447791a34a047590355abf356
kimminjeong <69973291+kim-minjeong01@users.noreply.github.com>
2022-01-27 02:36:08 +0900

    fragment recyclerview image

---
+++ app/src/main/java/org/gyeongsoton/aniverse_front/AdoptListRecycler.java
@@ -0,0 +1,150 @@
+package org.gyeongsoton.aniverse_front;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+
+public class AdoptListRecycler extends Fragment {
+
+    private ImageView ani_img1;
+    private TextView ani_info1;
+    String animalImage, animalSpecies, animalAge;
+    AnimalListRecyclerFragment animalListrecyclerfragment;
+    RecyclerView recyclerView;
+    AnimalListRecycleAdapter aniAdapter;
+    ArrayList<AnimalListRecyclerItem> mList;
+    private Context mContext;
+
+
+    //프래그먼트를 액티비티 위에 올린다.
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        animalListrecyclerfragment = (AnimalListRecyclerFragment) getActivity();
+        System.out.println("입양: onAttach 실행");
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        System.out.println("입양: onDestroyView 실행");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        System.out.println("입양: onDestroy 실행");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_animallistrecycler, container, false); //프래그먼트 레이아웃을 클래스에 올려줌
+        System.out.println("입양: onCreateView 실행");
+
+        Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler()); //예외처리 핸들러
+
+        //현재 상태 저장, 새 리스트
+        mContext = getContext();
+        mList = new ArrayList<>();
+        //어댑터 객체 (현재 상태와 리스트 전달)
+        aniAdapter = new AnimalListRecycleAdapter(mContext,mList);
+        //리사이클러뷰 객체
+        recyclerView = (RecyclerView) view.findViewById(R.id.aniRecyclerView);
+        recyclerView.setAdapter(aniAdapter);
+        //레이아웃 지정
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("isSuccess"); //response 제대로 왔는지 확인
+
+                    if (success) {
+                        System.out.println("성공");
+
+                        JSONArray respArr = (JSONArray) jsonResponse.get("adoptListRows");
+                        System.out.println(respArr.length());
+
+
+                        mList = new ArrayList<>();
+                        //어댑터 객체
+                        aniAdapter = new AnimalListRecycleAdapter(mContext,mList);
+                        //리사이클러뷰 객체
+                        recyclerView = (RecyclerView) view.findViewById(R.id.aniRecyclerView);
+                        recyclerView.setAdapter(aniAdapter);
+                        //레이아웃 지정
+                        recyclerView.setLayoutManager(new GridLayoutManager(container.getContext(), 2));
+
+                        for(int i=0;i<respArr.length();i++){
+                            AnimalListRecyclerItem item= new AnimalListRecyclerItem();
+                            JSONObject obj = null;
+                            try {
+                                obj = (JSONObject)respArr.get(i);
+                                item.setImage(obj.getString("animalImage"));
+                                item.setInfo(obj.getString("animalSpecies")+" "+obj.getString("animalAge"));
+                                aniAdapter.setArrayData(item);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        animallist_Request request = new animallist_Request("S",responseListener);
+        RequestQueue queue = Volley.newRequestQueue(container.getContext());
+        queue.add(request);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        System.out.println("입양: onViewCreated 실행 : before super");
+        super.onViewCreated(view, savedInstanceState);
+        System.out.println("입양: onViewCreated 실행 : after super");
+
+    }
+}
---
+++ app/src/main/java/org/gyeongsoton/aniverse_front/AnimalListRecyclerFragment.java
@@ -0,0 +1,194 @@
+package org.gyeongsoton.aniverse_front;
+
+import static android.content.ContentValues.TAG;
+
+import static java.lang.Thread.sleep;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.bumptech.glide.Glide;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class AnimalListRecyclerFragment extends AppCompatActivity {
+
+    private ImageView ani_1;
+    private TextView textView1;
+
+    //프래그먼트 식별 변수
+    private final int Fragment_1 = 1;
+    private final int Fragment_2 = 2;
+    private final int Fragment_3 = 3;
+
+    //프래그먼트 객체 생성(객체 변수를 전역변수로 만들며 프래그먼트 오류 해결) //Animal Activity가 생성되면서 그 위에 올라갈 프래그먼트들도 함께 생성
+    Fragment fragment1=new AdoptListRecycler();
+    Fragment fragment2=new ProtectListRecycler();
+    Fragment fragment3=new CompleteListRecycler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_animallist);
+
+        Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler());
+
+        Button add_btn = (Button)findViewById(R.id.add_btn);
+
+        //AdoptList, ProtectList upload가 서로 다름
+    /*    add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Adopt_protect_upload.class);
+
+                startActivity(intent);
+            }
+        });*/
+
+        ImageButton adopt_btn = (ImageButton)findViewById(R.id.adopt_btn);
+        adopt_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AnimalList.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton home_btn = (ImageButton)findViewById(R.id.home_btn);
+        home_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Main.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton funding_btn = (ImageButton)findViewById(R.id.funding_btn);
+        funding_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FundingList.class);
+                startActivity(intent);
+            }
+        });
+        ImageButton market_btn = (ImageButton)findViewById(R.id.market_btn);
+        market_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),AnimalListRecycler.class);
+                startActivity(intent);
+            }
+        });
+        ImageButton mypage_btn = (ImageButton)findViewById(R.id.mypage_btn);
+        mypage_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Mypage.class);
+                startActivity(intent);
+            }
+        });
+
+        /*입양, 입양완료 버튼 전환*/
+        Button adopt_tab = (Button)findViewById(R.id.adopt_tab);
+        Button protect_tab = (Button)findViewById(R.id.protect_tab);
+        Button complete_tab = (Button)findViewById(R.id.complete_tab);
+
+        adopt_tab.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //fragment1 = new AdoptList();
+                //sleep(3);
+                FragmentView(Fragment_1);
+                adopt_tab.setPressed(true);
+                protect_tab.setPressed(false);
+                complete_tab.setPressed(false);
+                //ResponseListener와 Volley 가 있는 클래스 호출 (어떤 프래그먼트 호출할건지 알려줄만한 정보, Volley요청 시 필요한 인자 함께 전달)
+                return true;
+            }
+        });
+
+        protect_tab.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //fragment2 = new ProtectList();
+                FragmentView(Fragment_2);
+                protect_tab.setPressed(true);
+                adopt_tab.setPressed(false);
+                complete_tab.setPressed(false);
+                return true;
+
+            }
+        });
+
+        complete_tab.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //fragment3 = new CompleteList();
+                FragmentView(Fragment_3);
+                complete_tab.setPressed(true);
+                adopt_tab.setPressed(false);
+                protect_tab.setPressed(false);
+                return true;
+
+            }
+        });
+
+        //기본상태(뺄지 말지 고민중)
+        //adopt_tab.setPressed(true);
+        //protect_tab.setPressed(false);
+        //complete_tab.setPressed(false);
+
+    }
+
+
+    /*프래그먼트 버튼 클릭 시 동작*/
+    private void FragmentView(int fragment) {
+        switch (fragment) { //입양
+            case 1:
+                //Fragment fragment1 = new AdoptList();
+                getSupportFragmentManager().beginTransaction().replace(R.id.aniaml_list_container,fragment1).commit(); //프래그먼트 변경 //변경사항 반영 되나?....
+                break;
+
+            case 2: //임시보호
+                //Fragment fragment2 = new ProtectList();
+                getSupportFragmentManager().beginTransaction().replace(R.id.aniaml_list_container,fragment2).commit();
+                break;
+
+            case 3: //완료
+                //Fragment fragment3 = new CompleteList();
+                getSupportFragmentManager().beginTransaction().replace(R.id.aniaml_list_container,fragment3).commit();
+                break;
+        }
+    }
+
+}
