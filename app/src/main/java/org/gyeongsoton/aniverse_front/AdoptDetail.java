487529b0fd9d366c62d2a8feba128959f97cff32
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-02-04 21:48:23 +0900

    AdoptAnimalUpload.java and adoptanimalupload_Request.java 수정 _Requet 코드는 url만 변경
    => 임시보호 동물 업로드도 동일하게 바꿀 것!

--- app/src/main/java/org/gyeongsoton/aniverse_front/AdoptDetail.java
+++ app/src/main/java/org/gyeongsoton/aniverse_front/AdoptDetail.java
@@ -14,8 +14,8 @@ import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
+//import com.android.volley.VolleyError;
+//import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 
